2c3e83f4a7f8abcbde5098ba51829ee64c0e0bbc
LARK-EnDN <nuyyynuii@gmail.com>
2020-10-20 01:43:07 +0700

    test read DB

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: TabBarDemo(),
-    );
-  }
-}
-
-class TabBarDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -29,25 +15,47 @@ class TabBarDemo extends StatelessWidget {
           appBar: AppBar(
             bottom: TabBar(
               tabs: [
-                Tab(icon:  Icon(Icons.person)),
-                Tab(icon:  Icon(Icons.pregnant_woman)),
-                Tab(icon:  Icon(Icons.accessible)),
-                Tab(icon:  Icon(Icons.work)),
-                Tab(icon:  Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.person)),
+                Tab(icon: Icon(Icons.pregnant_woman)),
+                Tab(icon: Icon(Icons.accessible)),
+                Tab(icon: Icon(Icons.work)),
+                Tab(icon: Icon(Icons.directions_car)),
               ],
             ),
-            title: Text('Parking status'),
+            title: Text('Parking Status'),
           ),
           body: TabBarView(
-            children: [
-              Icon(Icons.person),
-              Icon(Icons.pregnant_woman),
-              Icon(Icons.accessible),
-              Icon(Icons.work),
-              Icon(Icons.directions_car),
-          ],),
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.all(20.0),
+                child: Text('ว่าง'),
+              ),
+              Container(
+                margin: const EdgeInsets.all(20.0),
+                child: RaisedButton (
+                  child: Text('read'),
+                  onPressed: () {
+                    //readdata();
+                  }),
+              ),
+              Text('พิการ'),
+              Text('VIP'),
+              Text('ทั้งหมด'),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+/*
+  void readdata() {
+    REF.child('status_and_name/building_1/floor_1/sensor').once().then((DataSnapshot dataSnapShot){
+      print(dataSnapShot.value);
+      /*Map<dynamic, dynamic> values = dataSnapShot.value;
+      values.forEach((key, value) {
+        print(value['1']);
+      });*/
+    });
+  }*/
+
