d8cc62fa130af0ec20e2ead561295fb727b48f47
LARK-EnDN <nuyyynuii@gmail.com>
2020-10-21 09:22:27 +0700

    onValue

--- lib/main.dart
+++ lib/main.dart
@@ -5,57 +5,116 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final Ref = FirebaseDatabase.instance.reference();
+  String text = ' ';
+
+  @override
+  void initState() {
+    super.initState();
+    readAllData();
+  }
+
+  Future<void> readAllData() async {
+    await Ref.child('status_and_name/building_1/floor_1/sensor/1')
+        .onValue
+        .listen((b) {
+      var snapshot = b.snapshot;
+      var fullname = snapshot.value['name'].split('_');
+      
+      //List<DataSnapshot> b = snapshot;
+
+
+      print(text);
+      setState(() {
+        text = fullname[3];
+      });
+    });
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.indigo,
+      ),
       home: DefaultTabController(
         length: 5,
         child: Scaffold(
           appBar: AppBar(
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.person)),
-                Tab(icon: Icon(Icons.pregnant_woman)),
-                Tab(icon: Icon(Icons.accessible)),
-                Tab(icon: Icon(Icons.work)),
-                Tab(icon: Icon(Icons.directions_car)),
-              ],
-            ),
             title: Text('Parking Status'),
           ),
+          bottomNavigationBar: manu(),
           body: TabBarView(
             children: <Widget>[
-              Container(
-                margin: const EdgeInsets.all(20.0),
-                child: Text('ว่าง'),
-              ),
-              Container(
-                margin: const EdgeInsets.all(20.0),
-                child: RaisedButton (
-                  child: Text('read'),
-                  onPressed: () {
-                    //readdata();
-                  }),
-              ),
-              Text('พิการ'),
-              Text('VIP'),
-              Text('ทั้งหมด'),
+              persontab(),
+              womentab(),
+              accessibletab(),
+              viptab(),
+              all(),
             ],
           ),
         ),
       ),
     );
   }
-}
-/*
-  void readdata() {
-    REF.child('status_and_name/building_1/floor_1/sensor').once().then((DataSnapshot dataSnapShot){
-      print(dataSnapShot.value);
-      /*Map<dynamic, dynamic> values = dataSnapShot.value;
-      values.forEach((key, value) {
-        print(value['1']);
-      });*/
-    });
-  }*/
 
+  Widget persontab() {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Text('$text'),
+    );
+  }
+
+  Widget womentab() {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Text('tab 2'),
+    );
+  }
+
+  Widget accessibletab() {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Text('tab 3'),
+    );
+  }
+
+  Widget viptab() {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Text('tab 4'),
+    );
+  }
+
+  Widget all() {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Text('tab 5'),
+    );
+  }
+
+  Widget manu() {
+    return Container(
+      color: Colors.indigo,
+      padding: EdgeInsets.all(5.0),
+      child: TabBar(
+        unselectedLabelColor: Colors.white54,
+        indicatorSize: TabBarIndicatorSize.tab,
+        indicatorPadding: EdgeInsets.all(5.0),
+        indicatorColor: Colors.white,
+        tabs: [
+          Tab(icon: Icon(Icons.person)),
+          Tab(icon: Icon(Icons.pregnant_woman)),
+          Tab(icon: Icon(Icons.accessible)),
+          Tab(icon: Icon(Icons.work)),
+          Tab(icon: Icon(Icons.directions_car)),
+        ],
+      ),
+    );
+  }
+}
